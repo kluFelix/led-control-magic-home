eef312dbdb7e32adfa182df37b64c1d4d447316f
Felix Klumpp <felix-k@gmx.com>
2026-04-02 10:05:15 +0200

    Add build option to nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Go development environment for LED controller server";
+  description = "LED Controller Server";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -9,16 +9,33 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      default = pkgs.buildGoModule rec {
+        pname = "led-server";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = null;
+        ldflags = [ "-s" "-w" ];
+      };
     in {
-      devShells.${system}.default = pkgs.mkShell {
+      packages.x86_64-linux.default = default;
+
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${default}/bin/led-server";
+      };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           go
           gopls
         ];
 
         shellHook = ''
-          echo "Go development environment ready!"
+          echo "LED Server dev environment ready!"
           echo "Go version: $(go version)"
+          echo "Run 'nix build' to build the binary"
+          echo "Binary will be at: $(nix build --print-out-paths)/bin/led-server"
         '';
       };
     };
